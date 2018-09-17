6eba4ca5437ffe064aff3aa77e1ecda0399ed00d
Michaela DeForest <mdeforest@galileosuite.com>
2018-09-17 11:29:03 -0400

    Adjust text for easier editing

--- index.php
+++ index.php
@@ -11,19 +11,17 @@
   </head>
   <body>
     <div class="container">
-      <div class="top-container">
-        <h1>Michaela DeForest</h1>
-        <img src="images/profile.jpg" alt="Picture of me">
-        <section>
-          <h2>About Me</h2>
-          <p>My name is Michaela DeForest. I currently work as a software developer in Malvern, Pennsylvania and am living in Philadelphia.
-          I graduated with a BA in Computer Science in 2017 and am beginning the path of gaining my Master's in Software Engineering.</p>
-          <p>While I am taking this class to fulfill a requirement, I chose it in hopes that it helps me in my job. 
-          I have had to learn a lot about web application development on my own while building a few Ruby on Rails apps for work. 
-          I am interested in gaining some formal web application development skills.</p>
-          <p>My hobbies include playing violin, playing basketball, and attending hackathons.</p>
-        </section>
-      </div>
+      <h1>Michaela DeForest</h1>
+      <img src="images/profile.jpg" alt="Picture of me">
+      <section>
+        <h2>About Me</h2>
+        <p>My name is Michaela DeForest. I currently work as a software developer in Malvern, Pennsylvania and am living in Philadelphia.
+           I graduated with a BA in Computer Science in 2017 and am beginning the path of gaining my Master's in Software Engineering.</p>
+        <p>While I am taking this class to fulfill a requirement, I chose it in hopes that it helps me in my job.
+           I have had to learn a lot about web application development on my own while building a few Ruby on Rails apps for work.
+           I am interested in gaining some formal web application development skills.</p>
+        <p>My hobbies include playing violin, playing basketball, and attending hackathons.</p>
+      </section>
       <section>
         <h2>Random Quote</h2>
         <blockquote>
